28b8587001311c3fe4e215c90f6c236171cb5f61
Droped <droped17@gmail.com>
2025-06-22 10:24:34 +0700

    [create]: initial Vue project

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<!-- TS -->
+<script setup lang="ts">
+</script>
+
+<!-- HTML -->
+<template>
+  <div class="bg-red-300">
+    <p>test</p>
+  </div>
+</template>
+
+<!-- CSS -->
+<style scoped>
+</style>
